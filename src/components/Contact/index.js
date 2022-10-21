74e9b32217bae12de4e4924f6393dd747883db6e
Devi <rmdn321@gmail.com>
2022-10-20 22:48:04 -0700

    Added the contact me and resume pages

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import './index.css'
 
 const Contact = () => {
   return (
     <div>
-      Contact
+    <div className='container d-flex justify-content-center mt-5'>
+      <div className="card contact-card mb-3">
+        <div className="row g-0">
+          <h2 className="text-center text-pink">Contact Me</h2>
+        <div className="mb-3">
+          <label for="name" className="form-label">Name</label>
+          <input type="text" className="form-control"/>
+        </div>
+        <div className="mb-3">
+          <label for="email" className="form-label">Email address</label>
+          <input type="email" className="form-control"/>
+        </div>
+        <div className="mb-3">
+          <label for="message" className="form-label">Message</label>
+          <textarea className="form-control" rows="3"></textarea>
+        </div> 
+        <button className='btn pink-btn'>Send</button>        
+        </div>
+      </div>
     </div>
+  </div>
   )
 }
 
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Resume = () => {
   return (
     <div>
-      Resume
+      <div className="container d-flex justify-content-center mt-5">
+        <div className="card about-card mb-3">
+          <div className="row g-0">
+            <h2 className="text-pink mb-4">I am proficient in the following...</h2>
+            <div className="col-md-6">
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+                <li>JQuery</li>
+                <li>Bootstrap</li>
+                <li>Node.js</li>
+                <li>Express.js</li>
+              </ul>              
+            </div>
+            <div className="col-md-6">
+            <ul>               
+                <li>MySQL</li>
+                <li>MongoDB</li>
+                <li>Sequelize</li>
+                <li>Mongoose</li>
+                <li>React.js</li>
+                <li>GraphQL</li>  
+                <li>Redux</li>             
+              </ul>
+            </div>
+            <button className='btn pink-btn'>Download Resume</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Resume
+export default Resume;
